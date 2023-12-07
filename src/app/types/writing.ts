27dda436cf5ea194aa9bfd2a5c6bb599ffc3c5f4
Federica Donald <federica.donald@ibm.com>
2023-12-07 20:09:13 +0000

    saving writing

--- src/app/types/writing.ts
+++ src/app/types/writing.ts
@@ -1,6 +1,8 @@
 export type Writing = {
     id: string
+    user_id?: string
     date: string
     quote: string
     piece: string
+    state: string
 }
--- src/features/dailyPrompt/promptDashboard/WritingArea.tsx
+++ src/features/dailyPrompt/promptDashboard/WritingArea.tsx
@@ -2,20 +2,37 @@ import React, { ChangeEvent, useState } from 'react'
 import { Button, Form, TextArea } from 'semantic-ui-react'
 import { Writing } from '../../../app/types/writing'
 import { createId } from '@paralleldrive/cuid2'
+import { doc, setDoc, updateDoc } from 'firebase/firestore'
+import { db } from '../../../app/config/firebase'
+import { useAppSelector } from '../../../app/store/store'
 
 const WritingArea = (props: { date: string; quote: string}) => {
+  const {currentUser} = useAppSelector(state => state.auth)
   const [writing, setWriting] = useState('')
+  const [loading, setLoading] = useState(false)
   const [wordCount, setWordCount] = useState(0)
   const [dailyWriting, setDailyWriting] = useState<Writing>()
 
-  function onSubmit(){
+  async function onSubmit(){
+    setLoading(true)
+    const user_id = currentUser?.uid ? currentUser?.uid : 'nulls'
     setDailyWriting({
         id: dailyWriting ? dailyWriting.id : createId(),
+        user_id: currentUser?.uid,
         date: props.date,
         quote: props.quote,
-        piece: writing
+        piece: writing,
+        state: dailyWriting ? "update" : "new"
     })
-    console.log(dailyWriting)
+    if (dailyWriting?.state === "new"){
+      await setDoc(doc(db, "writing", user_id), dailyWriting);
+      setLoading(false)
+    } else {
+      await updateDoc(doc(db, "writing", user_id), {
+        piece: writing
+      });
+      setLoading(false)
+    }
   }
 
   function handleInputChange(e: ChangeEvent<HTMLTextAreaElement>){
@@ -28,7 +45,7 @@ const WritingArea = (props: { date: string; quote: string}) => {
     <Form>
         <TextArea rows={20} placeholder='Write your story' onChange={e => handleInputChange(e)} />
         <p>{wordCount} words</p>
-        <Button style={{marginTop:'1rem'}} type='submit' onClick={onSubmit}>Save</Button>
+        <Button loading={loading} style={{marginTop:'1rem'}} type='submit' onClick={onSubmit}>Save</Button>
     </Form>
   )
 }
